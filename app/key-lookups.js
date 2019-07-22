0837b24e7826c11c91e0a97de3eca1d1afabdf05
David M Sibley <sibley@wisc.edu>
2019-07-22 11:33:30 -0500

    refactor map and add router

---
+++ app/key-lookups.js
@@ -0,0 +1,33 @@
+export const ignoredKeys = [
+  'SiteName',
+  'Site_Name',
+  'Wid',
+  'ID'
+];
+export const keyLookup = {
+  'SiteName': {title: 'Site Name', desc: ''},
+  'Site_Name': {title: 'Site Name', desc: ''},
+  'Wid': {title: 'WID', desc: ''},
+  'ID': {title: 'ID', desc: ''},
+  'RecentLog': {title: 'Most recent log', desc: ''},
+  'MaxDepth': {title: 'Max depth (ft)', desc: ''},
+  'Norm_Res': {title: 'Normal Resistivity', desc: ''},
+  'Caliper': {title: 'Caliper', desc: ''},
+  'Gamma': {title: 'Natural Gamma', desc: ''},
+  'SP': {title: 'Spontaneous (Self) Potential', desc: ''},
+  'SPR': {title: 'Single Point Resistivity', desc: ''},
+  'Spec_Gamma': {title: 'Spectral Gamma', desc: ''},
+  'Fluid_Cond': {title: 'Fluid Conductivity', desc: ''},
+  'Flow_Spin': {title: 'Spinner Flow Meter', desc: ''},
+  'Fluid_Temp': {title: 'Fluid Temperature', desc: ''},
+  'Fluid_Res': {title: 'Fluid Resistivity', desc: ''},
+  'OBI': {title: 'Optical Borehole Image (OBI)', desc: ''},
+  'ABI': {title: 'Acoustic Borehole Image (ABI)', desc: ''},
+  'Video': {title: 'Video', desc: ''},
+  'Drill_Year': {title: 'Drill year', desc: ''},
+  'Depth_Ft': {title: 'Depth (ft)', desc: ''},
+  'Drill_Meth': {title: 'Drill Method', desc: ''},
+  'Subsamples': {title: 'Subsamples', desc: ''},
+  'Photos': {title: 'Core Photos', desc: ''},
+  'Grainsize': {title: 'Grainsize', desc: ''},
+};
---
+++ app/site-map.js
@@ -0,0 +1,172 @@
+import { RestylingCircleMarker } from './restyling-circle-marker.js';
+
+export class SiteMap extends window.L.Evented {
+  constructor() {
+    super();
+    this.selected = false;
+    this._highlight = null;
+
+    /* ~~~~~~~~ Map ~~~~~~~~ */
+    //create a map, center it, and set the zoom level. 
+    //set zoomcontrol to false because we will add it in a different corner. 
+    const map = this.map = L.map('map', {zoomControl:false}).setView([45, -89.623861], 7);
+    this.el = document.querySelector('#map');
+     
+     /* ~~~~~~~~ Zoom Control ~~~~~~~~ */
+    //place a zoom control in the top right: 
+    new L.Control.Zoom({position: 'topright'}).addTo(map);
+
+     
+    /* ~~~~~~~~ Basemap Layers ~~~~~~~~ */
+     
+    // basemaps from Open Street Map
+    const osmhot = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors', 
+      label: "OpenStreetMap Humanitarian"
+    }).addTo(map);
+
+    // Esri basemaps 
+    const esrisat = L.esri.basemapLayer('Imagery', {label: "Esri Satellite"});
+    
+    // add the basemap control to the map  
+    var basemaps = [osmhot, esrisat]; 
+    map.addControl(L.control.basemaps({
+       basemaps: basemaps, 
+       tileX: 0, 
+       tileY: 0, 
+       tileZ: 1
+    })); 
+
+    /* +++++++++++ Borehole Geophysical Logs layer +++++++++++ */ 
+    let bore = this.bore = L.esri.featureLayer({
+      url: "https://data.wgnhs.wisc.edu/arcgis/rest/services/geologic_data/borehole_geophysics/MapServer/0",
+      pointToLayer: function(geoJsonPoint, latlon) {
+        return new RestylingCircleMarker(latlon, {
+          weight: 2,
+          radius: RestylingCircleMarker.calcRadius(map.getZoom())
+        });
+      }
+    }).on('click', (function(e) {
+      if (this._highlight !== e.propagatedFrom) {
+        this.fire('interaction', e.propagatedFrom.feature.properties);
+      } else {
+        this.fire('interaction');
+      }
+    }).bind(this));
+
+    /* +++++++++++ Sediment Core layer +++++++++++ */ 
+    let quat = this.quat = L.esri.featureLayer({
+      url: "https://data.wgnhs.wisc.edu/arcgis/rest/services/geologic_data/sediment_core/MapServer/0",
+      pointToLayer: function(geoJsonPoint, latlon) {
+        return new RestylingCircleMarker(latlon, {
+          weight: 2,
+          color: '#33AA44',
+          radius: RestylingCircleMarker.calcRadius(map.getZoom())
+        });
+      }
+    }).on('click', (function(e) {
+      if (this._highlight !== e.propagatedFrom) {
+        this.fire('interaction', e.propagatedFrom.feature.properties);
+      } else {
+        this.fire('interaction');
+      }
+    }).bind(this));
+
+    this.layers = [bore, quat];
+
+    Promise.all([new Promise(function(resolve, reject) {
+      bore.once('load', (function() {
+        resolve();
+      }))
+    }),new Promise(function(resolve, reject) {
+      quat.once('load', (function() {
+        resolve();
+      }))
+    })]).then(function() {
+      this.fire('init');
+    }.bind(this));
+
+    bore.addTo(map);
+    quat.addTo(map);
+  }
+
+  static getSiteCode(params) {
+    let keys = ['Wid', 'ID', 'Site_Code'];
+    let result = keys.reduce((prev, curr) => {
+      return prev || params[curr];
+    }, undefined)
+    return result;
+  }
+
+  getPoint(params) {
+    let result;
+
+    this.layers.forEach(function(layer) {
+      layer.eachFeature(function(obj) {
+        if (!result && SiteMap.getSiteCode(obj.feature.properties) === SiteMap.getSiteCode(params)) {
+          result = obj;
+        }
+      });
+    })
+    return result;
+  }
+
+  zoomToPoint(site) {
+    let point = this.getPoint(site);
+    if (point) {
+      this.map.setZoomAround(point.getLatLng(), 15);
+    }
+  }
+
+  getHighlightPoint() {
+    // console.log('retrieve highlight point');
+    let result = this._highlight;
+    return result;
+  }
+
+  setHighlightPoint(point) {
+    if (point) {
+      // console.log('set highlight point');
+      this._highlight = point;
+      this._highlight.bringToFront()
+      this._highlight.highlight();
+    } else {
+      this.clearSelection();
+    }
+  }
+
+  selectPoint(site) {
+    let result = null;
+    // console.log('select point on map:', site);
+    let point = this.getPoint(site);
+    if (point) {
+      result = point.feature.properties;
+      let highlightPoint = this.getHighlightPoint();
+      if (point !== highlightPoint) {
+        this.clearSelection();
+        this.setHighlightPoint(point);
+      }
+    }
+    return result;
+  }
+
+  clearSelection() {
+    // console.log('clear highlight group');
+    if (this._highlight) {
+      this._highlight.bringToBack();
+      this._highlight.removeHighlight();
+    }
+    this._highlight = null;
+  }
+
+  setVisibility(isVisible) {
+    if (isVisible) {
+      this.el.removeAttribute('data-closed');
+      this.map.invalidateSize();
+    } else {
+      this.el.setAttribute('data-closed', true);
+    }
+  }
+
+
+}
