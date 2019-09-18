15d9ca3f632150f01053e0a7c041926e3675f44a
WGNHS Jenkins <devops@wgnhs.wisc.edu>
2019-09-18 18:33:05 +0000

    Updates

--- app/index.js
+++ app/index.js
@@ -865,6 +865,83 @@
   }
   customElements.define('table-layout', TableLayout);
 
+  /**
+   * https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string
+   * @param {*} a 
+   * @param {*} b 
+   * @param {*} c 
+   * @param {*} d 
+   * @param {*} e 
+   */
+  const fileSizeIEC = function(a,b,c,d,e){
+    return (b=Math,c=b.log,d=1024,e=c(a)/c(d)|0,a/b.pow(d,e)).toFixed(2)
+    +' '+(e?'KMGTPEZY'[--e]+'iB':'Bytes')
+   };
+
+  class DownloadButton extends litElement.LitElement {
+    static get properties() {
+      return {
+        src: {
+          type: String
+        },
+        exists: {
+          type: Boolean,
+          attribute: false
+        },
+        fileSize: {
+          type: String,
+          attribute: false
+        }
+      };
+    }
+
+    constructor() {
+      super();
+    }
+
+    static get styles() {
+      return [
+        ...wgnhsCommon.styles,
+        litElement.css`
+      [data-closed] {
+        display: none;
+      }
+      .file-size {
+        font-size: var(--font-size-small);
+      }
+    `];
+    }
+
+    render() {
+      return litElement.html`
+    <button-link href="${this.src}" target="_blank" download ?data-closed="${!this.exists}">
+      <i slot="content-before" class="material-icons" title="Download">save_alt</i>
+      <span slot="content"><slot>Download</slot></span>
+      <span slot="content-after" class="file-size">${this.fileSize}</span>
+    </button-link>
+    `;
+    }
+
+    updated(prev) {
+      if (prev.has('src') && this.src) {
+        this.exists = false;
+        fetch(
+          this.src, 
+          {method: 'HEAD'}
+          ).then(resp => {
+            if (resp.ok) {
+              this.exists = true;
+              let bytes = resp.headers.get('Content-Length');
+              if (bytes) {
+                this.fileSize = fileSizeIEC(bytes);
+              }
+            }
+          });
+      }
+    }
+  }
+  customElements.define('download-button', DownloadButton);
+
   class LogLayout extends litElement.LitElement {
     static get layoutName() {
       return 'geophysical log';
@@ -899,9 +976,13 @@
     <table-layout .info=${this.prepInfo()} .context=${this.context}></table-layout>
     <pdf-view-button
       .panel=${this.context.pdfpanel}
-      src="${'https://data.wgnhs.wisc.edu/geophysical-logs/' + this.info.Wid + '.pdf'}">
+      src="${'https://data.wgnhs.wisc.edu/borehole-geophysics/pdf/' + this.info.Wid + '.pdf'}">
       <span slot="download-text">Download PDF</span>
     </pdf-view-button>
+    <download-button
+      src="${'https://data.wgnhs.wisc.edu/borehole-geophysics/las/' + this.info.Wid + '.las'}">
+      Download LAS
+    </download-button>
     `;
     }
 
