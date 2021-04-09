9fdf66bd389486d2efcea096bdc406f194ada660
David M Sibley <sibley@wisc.edu>
2021-04-09 11:46:36 -0500

    use normal people units for download button

--- app/details/download-button.js
+++ app/details/download-button.js
@@ -10,8 +10,8 @@ import { styles } from 'wgnhs-common';
  * @param {*} e 
  */
 const fileSizeIEC = function(a,b,c,d,e){
-  return (b=Math,c=b.log,d=1024,e=c(a)/c(d)|0,a/b.pow(d,e)).toFixed(2)
-  +' '+(e?'KMGTPEZY'[--e]+'iB':'Bytes')
+  return (b=Math,c=b.log,d=1024,e=c(a)/c(d)|0,a/b.pow(d,e)).toFixed(0)
+  +' '+(e?'KMGTPEZY'[--e]+'B':'Bytes')
  }
 
 export class DownloadButton extends LitElement {
