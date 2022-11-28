06f29133b448c22bb5aae80080056d94f37975ee
David M Sibley <sibley@wisc.edu>
2022-11-28 12:02:28 -0600

    Updates

--- app/index.js
+++ app/index.js
@@ -795,6 +795,13 @@
           // "Completeness": {},
           // "LonDD": {},
           // "LatDD": {},
+          "HasCoreImage": {
+            title: 'Core Image Available',
+            hidden: true,
+            controls: [
+              new CheckboxControl()
+            ]
+          }
         }
       }]
     })
@@ -1377,11 +1384,89 @@
   }
   customElements.define('quat-core-layout', QuatCoreLayout);
 
+  class RockCoreLayout extends litElement.LitElement {
+    static get layoutName() {
+      return 'rock core';
+    }
+
+    static include(info, context) {
+      return litElement.html`<rock-core-layout .info=${info} .context=${context}></rock-core-layout>`;
+    }
+
+    static get properties() {
+      return {
+        info: {
+          type: Object
+        },
+        context: {
+          type: Object
+        }
+      };
+    }
+
+    constructor() {
+      super();
+    }
+
+    static get styles() {
+      return litElement.css`
+    `;
+    }
+
+    render() {
+      return litElement.html`
+    <table-layout .info=${this.prepInfo()} .context=${this.context}></table-layout>
+    <pdf-view-button
+      .panel=${this.context.pdfpanel}
+      src="${'https://data.wgnhs.wisc.edu/core-photos/' + this.info.WID + '.pdf'}">
+      <span slot="download-text">Download Photos</span>
+    </pdf-view-button>
+    `;
+    }
+
+    prepInfo() {
+      return Object.assign(this.topFields, this.bottomFields);
+    }
+
+    get topFields() {
+      return this.getFields(el => !el.bundled);
+    }
+
+    get bottomFields() {
+      const fields = this.getFields(el => el.bundled);
+      const names = Object.entries(fields)
+        .filter(kv => !!kv[1])
+        .map(kv => SiteData.getFieldTitle(kv[0], this.context.layoutName));
+      return {
+        'Data available:': names.map(val => litElement.html`${val}<br>`)
+      };
+    }
+
+    get group() {
+      return filterLookup.find(el => (el.prop && el[el.prop] === this.info[el.prop]));
+    }
+
+    getFields(fn) {
+      const result = {};
+      const sections = this.group.sections.filter(fn);
+    
+      sections.forEach(section => {
+        Object.entries(section.fields).forEach(kv => {
+          result[kv[0]] = this.info[kv[0]];
+        });
+      });
+    
+      return result;
+    }
+  }
+  customElements.define('rock-core-layout', RockCoreLayout);
+
   const defaultLayout = TableLayout;
   const availableLayouts = [
     defaultLayout,
     LogLayout,
-    QuatCoreLayout
+    QuatCoreLayout,
+    RockCoreLayout
   ];
 
   const layoutResolver = {
